fe6707a6a7fbe469e78355db8f72eefaf009795d
Jaya Winata <jayawinata100@gmail.com>
2025-05-23 10:09:54 +0700

    feat: add feature engineering in the data transformation pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,10 +10,51 @@ class DataTransformation:
 
     def handle_missing_values(self):
         data = pd.read_csv(self.config.data_path)
-        data.dropna(subset=['weather_code'], inplace=True)
+        logger.info(f"Null values:\n{data.isna().sum()}")
+        data.dropna(how='any', inplace=True)
         data.to_csv(os.path.join(self.config.root_dir, self.config.transformed_filename), index=False)
 
+        logger.info(f"Null values:\n{data.isna().sum()}")
         logger.info("Missing value handled successfully")
+
+    def feature_engineering(self):
+        weather_codes = {
+            0: "Clear sky",
+            1: "Mainly clear",
+            2: "Partly cloudy",
+            3: "Overcast",
+            45: "Fog",
+            48: "Depositing rime fog",
+            51: "Light drizzle",
+            53: "Moderate drizzle",
+            55: "Dense drizzle",
+            56: "Light freezing drizzle",
+            57: "Dense freezing drizzle",
+            61: "Slight rain",
+            63: "Moderate rain",
+            65: "Heavy rain",
+            66: "Light freezing rain",
+            67: "Heavy freezing rain",
+            71: "Slight snow fall",
+            73: "Moderate snow fall",
+            75: "Heavy snow fall",
+            77: "Snow grains",
+            80: "Slight rain showers",
+            81: "Moderate rain showers",
+            82: "Violent rain showers",
+            85: "Slight snow showers",
+            86: "Heavy snow showers",
+            95: "Thunderstorm: slight or moderate",
+            96: "Thunderstorm with slight hail",
+            99: "Thunderstorm with heavy hail",
+        }
+
+        data = pd.read_csv(os.path.join(self.config.root_dir, self.config.transformed_filename))
+        data['weather'] = data['weather_code'].apply(lambda x: weather_codes.get(x, "Unknown"))
+        data.drop(['weather_code', 'time'], axis=1, inplace=True)
+        data.to_csv(os.path.join(self.config.root_dir, self.config.transformed_filename), index=False)
+
+        logger.info('Feature engineering done successfully')
 
     def train_test_spliting(self):
         data = pd.read_csv(os.path.join(self.config.root_dir, self.config.transformed_filename))
